de05923700ca2bf70100f4299bcba50f4d65d0ea
meno1011 <39692905+meno1011@users.noreply.github.com>
2021-03-21 23:25:11 +0900

    #2.4 My first time run Express Server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hi!");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
